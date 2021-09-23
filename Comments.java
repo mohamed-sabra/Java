55bea4a5644786ee1fb5b807955e954189c90507
mosabra <mosabra@MOSABRA-LAP.lb.oracle.com>
2021-09-23 12:12:38 +0300

    Java code snippets

---
+++ Comments.java
@@ -0,0 +1,12 @@
+public class Comments {
+  public static void main(String[] args) {
+	// This is a single line comment before the code
+	/*
+		This is a multiple lines comment
+		*
+		*
+		*
+	*/
+    System.out.println("Comments"); //this is a single line comment after the code, it will not be executed.
+  }
+}
